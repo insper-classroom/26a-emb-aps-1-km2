681550d676886c996b9c2febb5901cdddbce6e77
MarianaSquillante <marianass13@al.insper.edu.br>
2026-04-08 10:32:48 -0300

    maquina de estados2

--- main/main.c
+++ main/main.c
@@ -14,7 +14,7 @@
 #include "lose.h"
 #include "music.h"
 
-
+// ================= PINOS =================
 #define btn_r 15
 #define btn_g 14
 #define btn_b 12
@@ -46,7 +46,7 @@
 #define NUM_BOTOES 4
 #define NUM_PONTOS 10
 
-
+// ================= ESTADOS DO JOGO =================
 typedef enum {
     ESTADO_AGUARDANDO_INICIO,
     ESTADO_REPRODUZINDO_SEQUENCIA,
@@ -57,12 +57,12 @@ typedef enum {
     ESTADO_DERROTA
 } EstadoJogo;
 
-
+// ================= ARRAYS =================
 static const int pontos[NUM_PONTOS] = { led_10, led_9, led_8, led_7, led_6, led_5, led_4, led_3, led_2, led_1};
 static const int botoes_pin[NUM_BOTOES] = {btn_r, btn_g, btn_b, btn_y};
 static const int leds_pin[NUM_BOTOES] = {led_r, led_g, led_b, led_y};
 
-
+// ================= ESTRUTURA DO JOGO =================
 typedef struct {
     int sequencia[MAX_SEQUENCIA];
     int tamanho;
@@ -79,7 +79,7 @@ typedef struct {
     uint32_t tempo_ultima_jogada;
 } JogoState;
 
-
+// ================= ESTRUTURA DE ÁUDIO =================
 typedef struct {
     volatile const uint8_t* som_atual;
     volatile uint32_t tamanho_atual;
@@ -88,50 +88,50 @@ typedef struct {
     volatile bool audio_loop;
 } AudioState;
 
-
+// ================= VARIÁVEIS PARA IRQ (necessárias) =================
 static AudioState g_audio = {0};
 volatile int flg_inicio = 0;
 volatile int flg_botao[NUM_BOTOES] = {0};
 volatile uint32_t ultimo_tempo_botao[NUM_BOTOES] = {0};
-static JogoState jogo_global;
+static JogoState* ptr_jogo_irq = NULL;
 
-
+// ================= PROTÓTIPOS =================
 void core1_entry(void);
 void btn_callback(uint gpio, uint32_t events);
 void botoes_init(void);
-void atualizar_pontuacao(void);
+void atualizar_pontuacao(JogoState* jogo);
 void limpar_pontuacao(void);
 void acender_led(int cor, int tempo_ms);
 void tocar_som_buzzer(int cor);
-void iniciar_timer(void);
-void cancelar_timer(void);
-void maquina_estados(void);
-void transicao_estado(EstadoJogo novo_estado);
+void iniciar_timer(JogoState* jogo);
+void cancelar_timer(JogoState* jogo);
+void maquina_estados(JogoState* jogo);
+void transicao_estado(JogoState* jogo, EstadoJogo novo_estado);
 
-
+// ================= TIMER =================
 int64_t timeout_callback(alarm_id_t id, void *user_data) {
-    (void)user_data;
-    if (jogo_global.estado_atual == ESTADO_AGUARDANDO_JOGADA) {
-        jogo_global.flg_timeout = 1;
+    JogoState* jogo = (JogoState*)user_data;
+    if ((*jogo).estado_atual == ESTADO_AGUARDANDO_JOGADA) {
+        (*jogo).flg_timeout = 1;
     }
     return 0;
 }
 
-void iniciar_timer(void) {
-    if (jogo_global.id_alarme != -1) {
-        cancel_alarm(jogo_global.id_alarme);
+void iniciar_timer(JogoState* jogo) {
+    if ((*jogo).id_alarme != -1) {
+        cancel_alarm((*jogo).id_alarme);
     }
-    jogo_global.id_alarme = add_alarm_in_ms(TEMPO_LIMITE_MS, timeout_callback, NULL, false);
+    (*jogo).id_alarme = add_alarm_in_ms(TEMPO_LIMITE_MS, timeout_callback, jogo, false);
 }
 
-void cancelar_timer(void) {
-    if (jogo_global.id_alarme != -1) {
-        cancel_alarm(jogo_global.id_alarme);
-        jogo_global.id_alarme = -1;
+void cancelar_timer(JogoState* jogo) {
+    if ((*jogo).id_alarme != -1) {
+        cancel_alarm((*jogo).id_alarme);
+        (*jogo).id_alarme = -1;
     }
 }
 
-
+// ================= PWM IRQ =================
 void pwm_interrupt_handler(void) {
     pwm_clear_irq(pwm_gpio_to_slice_num(AUDIO_PIN));
 
@@ -180,7 +180,7 @@ void init_audio(void) {
     pwm_init(slice, &config, true);
 }
 
-
+// ================= CORE 1 =================
 void core1_entry(void) {
     init_audio();
     while (1) {
@@ -197,7 +197,7 @@ void core1_entry(void) {
     }
 }
 
-
+// ================= HARDWARE =================
 void botoes_init(void) {
     for(int i = 0; i < NUM_PONTOS; i++) {
         gpio_init(pontos[i]);
@@ -216,16 +216,16 @@ void botoes_init(void) {
     gpio_set_dir(buzzer, GPIO_OUT);
 }
 
-
+// ================= JOGO =================
 void limpar_pontuacao(void) {
     for(int i = 0; i < NUM_PONTOS; i++) {
         gpio_put(pontos[i], 0);
     }
 }
 
-void atualizar_pontuacao(void) {
-    if (jogo_global.tamanho > 0 && jogo_global.tamanho <= NUM_PONTOS) {
-        gpio_put(pontos[jogo_global.tamanho - 1], 1);
+void atualizar_pontuacao(JogoState* jogo) {
+    if ((*jogo).tamanho > 0 && (*jogo).tamanho <= NUM_PONTOS) {
+        gpio_put(pontos[(*jogo).tamanho - 1], 1);
     }
 }
 
@@ -239,11 +239,13 @@ static inline int identificar_botao(uint gpio) {
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == GPIO_IRQ_EDGE_FALL) {
-        if (!jogo_global.rodando) {
+        if (ptr_jogo_irq == NULL) return;
+        
+        if (!(*ptr_jogo_irq).rodando) {
             flg_inicio = 1;
             return;
         }
-        if (jogo_global.estado_atual == ESTADO_AGUARDANDO_JOGADA && !jogo_global.btn_p) {
+        if ((*ptr_jogo_irq).estado_atual == ESTADO_AGUARDANDO_JOGADA && !(*ptr_jogo_irq).btn_p) {
             uint32_t tempo_atual = to_ms_since_boot(get_absolute_time());
             int botao_id = identificar_botao(gpio);
             if (botao_id >= 0 && (tempo_atual - ultimo_tempo_botao[botao_id] > DEBOUNCE_TIME_MS)) {
@@ -270,26 +272,26 @@ void tocar_som_buzzer(int cor) {
     }
 }
 
-void transicao_estado(EstadoJogo novo_estado) {
-    jogo_global.estado_atual = novo_estado;
+void transicao_estado(JogoState* jogo, EstadoJogo novo_estado) {
+    (*jogo).estado_atual = novo_estado;
 }
 
-void maquina_estados(void) {
+void maquina_estados(JogoState* jogo) {
     int i;
-    EstadoJogo estado = jogo_global.estado_atual;
+    EstadoJogo estado = (*jogo).estado_atual;
     
-    
+    // ESTADO_AGUARDANDO_INICIO
     if (estado == ESTADO_AGUARDANDO_INICIO) {
         if (flg_inicio) {
             flg_inicio = 0;
             srand(time(NULL));
-            jogo_global.tamanho = 0;
-            jogo_global.rodando = 1;
-            jogo_global.btn_p = 0;
-            jogo_global.flg_timeout = 0;
-            jogo_global.n_jogada = 0;
-            jogo_global.btn_pressionado = 0;
-            jogo_global.tempo_ultima_jogada = 0;
+            (*jogo).tamanho = 0;
+            (*jogo).rodando = 1;
+            (*jogo).btn_p = 0;
+            (*jogo).flg_timeout = 0;
+            (*jogo).n_jogada = 0;
+            (*jogo).btn_pressionado = 0;
+            (*jogo).tempo_ultima_jogada = 0;
             
             for(i = 0; i < NUM_BOTOES; i++) {
                 flg_botao[i] = 0;
@@ -299,96 +301,96 @@ void maquina_estados(void) {
             limpar_pontuacao();
             multicore_fifo_push_blocking(1);
             
-            jogo_global.sequencia[jogo_global.tamanho] = rand() % 4;
-            jogo_global.tamanho++;
-            jogo_global.indice_reproducao = 0;
+            (*jogo).sequencia[(*jogo).tamanho] = rand() % 4;
+            (*jogo).tamanho++;
+            (*jogo).indice_reproducao = 0;
             
-            transicao_estado(ESTADO_REPRODUZINDO_SEQUENCIA);
+            transicao_estado(jogo, ESTADO_REPRODUZINDO_SEQUENCIA);
         }
     }
-    
+    // ESTADO_REPRODUZINDO_SEQUENCIA
     else if (estado == ESTADO_REPRODUZINDO_SEQUENCIA) {
-        if (jogo_global.indice_reproducao < jogo_global.tamanho) {
-            int cor = jogo_global.sequencia[jogo_global.indice_reproducao];
+        if ((*jogo).indice_reproducao < (*jogo).tamanho) {
+            int cor = (*jogo).sequencia[(*jogo).indice_reproducao];
             acender_led(cor, 300);
             multicore_fifo_push_blocking(10 + cor);
             sleep_ms(200);
-            jogo_global.indice_reproducao++;
+            (*jogo).indice_reproducao++;
         } else {
-            jogo_global.aguardando_jogada = 1;
-            jogo_global.btn_p = 0;
-            jogo_global.n_jogada = 0;
+            (*jogo).aguardando_jogada = 1;
+            (*jogo).btn_p = 0;
+            (*jogo).n_jogada = 0;
             
-            iniciar_timer();
+            iniciar_timer(jogo);
             
             for(i = 0; i < 4; i++) {
                 flg_botao[i] = 0;
             }
             
-            transicao_estado(ESTADO_AGUARDANDO_JOGADA);
+            transicao_estado(jogo, ESTADO_AGUARDANDO_JOGADA);
         }
     }
-    
+    // ESTADO_AGUARDANDO_JOGADA
     else if (estado == ESTADO_AGUARDANDO_JOGADA) {
-        if (jogo_global.flg_timeout) {
-            jogo_global.flg_timeout = 0;
-            transicao_estado(ESTADO_DERROTA);
+        if ((*jogo).flg_timeout) {
+            (*jogo).flg_timeout = 0;
+            transicao_estado(jogo, ESTADO_DERROTA);
         } else {
             for(i = 0; i < 4; i++) {
                 if (flg_botao[i]) {
-                    jogo_global.btn_p = 1;
-                    jogo_global.btn_pressionado = i;
-                    jogo_global.tempo_ultima_jogada = to_ms_since_boot(get_absolute_time());
+                    (*jogo).btn_p = 1;
+                    (*jogo).btn_pressionado = i;
+                    (*jogo).tempo_ultima_jogada = to_ms_since_boot(get_absolute_time());
                     flg_botao[i] = 0;
                     
                     for(int j = 0; j < 4; j++) {
                         if (j != i) flg_botao[j] = 0;
                     }
                     
-                    transicao_estado(ESTADO_PROCESSANDO_JOGADA);
+                    transicao_estado(jogo, ESTADO_PROCESSANDO_JOGADA);
                     break;
                 }
             }
         }
     }
-    
+    // ESTADO_PROCESSANDO_JOGADA
     else if (estado == ESTADO_PROCESSANDO_JOGADA) {
-        i = jogo_global.btn_pressionado;
+        i = (*jogo).btn_pressionado;
         acender_led(i, 150);
         multicore_fifo_push_blocking(10 + i);
         sleep_ms(50);
         
-        if(i == jogo_global.sequencia[jogo_global.n_jogada]) {
-            jogo_global.n_jogada++;
+        if(i == (*jogo).sequencia[(*jogo).n_jogada]) {
+            (*jogo).n_jogada++;
             
-            if(jogo_global.n_jogada >= jogo_global.tamanho) {
-                atualizar_pontuacao();
+            if((*jogo).n_jogada >= (*jogo).tamanho) {
+                atualizar_pontuacao(jogo);
                 
-                if (jogo_global.tamanho >= 10) {
-                    transicao_estado(ESTADO_VITORIA);
+                if ((*jogo).tamanho >= 10) {
+                    transicao_estado(jogo, ESTADO_VITORIA);
                 } else {
-                    jogo_global.sequencia[jogo_global.tamanho] = rand() % 4;
-                    jogo_global.tamanho++;
-                    jogo_global.indice_reproducao = 0;
-                    jogo_global.n_jogada = 0;
-                    jogo_global.aguardando_jogada = 0;
-                    jogo_global.btn_p = 0;
+                    (*jogo).sequencia[(*jogo).tamanho] = rand() % 4;
+                    (*jogo).tamanho++;
+                    (*jogo).indice_reproducao = 0;
+                    (*jogo).n_jogada = 0;
+                    (*jogo).aguardando_jogada = 0;
+                    (*jogo).btn_p = 0;
                     sleep_ms(500);
                     
                     for(int j = 0; j < 4; j++) {
                         flg_botao[j] = 0;
                     }
                     
-                    transicao_estado(ESTADO_REPRODUZINDO_SEQUENCIA);
+                    transicao_estado(jogo, ESTADO_REPRODUZINDO_SEQUENCIA);
                 }
             } else {
-                transicao_estado(ESTADO_AGUARDANDO_PROXIMA);
+                transicao_estado(jogo, ESTADO_AGUARDANDO_PROXIMA);
             }
         } else {
-            transicao_estado(ESTADO_DERROTA);
+            transicao_estado(jogo, ESTADO_DERROTA);
         }
     }
-    
+    // ESTADO_AGUARDANDO_PROXIMA
     else if (estado == ESTADO_AGUARDANDO_PROXIMA) {
         sleep_ms(150);
         
@@ -396,81 +398,85 @@ void maquina_estados(void) {
             flg_botao[i] = 0;
         }
         
-        iniciar_timer();
-        jogo_global.btn_p = 0;
-        transicao_estado(ESTADO_AGUARDANDO_JOGADA);
+        iniciar_timer(jogo);
+        (*jogo).btn_p = 0;
+        transicao_estado(jogo, ESTADO_AGUARDANDO_JOGADA);
     }
-    
+    // ESTADO_VITORIA
     else if (estado == ESTADO_VITORIA) {
-        jogo_global.rodando = 0;
-        jogo_global.aguardando_jogada = 0;
-        jogo_global.btn_p = 0;
-        cancelar_timer();
+        (*jogo).rodando = 0;
+        (*jogo).aguardando_jogada = 0;
+        (*jogo).btn_p = 0;
+        cancelar_timer(jogo);
         
-        if (jogo_global.animacao_contador < 3) {
+        if ((*jogo).animacao_contador < 3) {
             for(i = 0; i < 4; i++) gpio_put(leds_pin[i], 1);
             sleep_ms(200);
             for(i = 0; i < 4; i++) gpio_put(leds_pin[i], 0);
             sleep_ms(200);
-            jogo_global.animacao_contador++;
+            (*jogo).animacao_contador++;
         } else {
             multicore_fifo_push_blocking(2);
-            jogo_global.animacao_contador = 0;
-            transicao_estado(ESTADO_AGUARDANDO_INICIO);
+            (*jogo).animacao_contador = 0;
+            transicao_estado(jogo, ESTADO_AGUARDANDO_INICIO);
         }
     }
-    
+    // ESTADO_DERROTA
     else if (estado == ESTADO_DERROTA) {
-        jogo_global.rodando = 0;
-        jogo_global.aguardando_jogada = 0;
-        jogo_global.btn_p = 0;
-        cancelar_timer();
+        (*jogo).rodando = 0;
+        (*jogo).aguardando_jogada = 0;
+        (*jogo).btn_p = 0;
+        cancelar_timer(jogo);
         limpar_pontuacao();
         
-        if (jogo_global.animacao_contador < 3) {
+        if ((*jogo).animacao_contador < 3) {
             for(i = 0; i < 4; i++) gpio_put(leds_pin[i], 1);
             sleep_ms(200);
             for(i = 0; i < 4; i++) gpio_put(leds_pin[i], 0);
             sleep_ms(200);
-            jogo_global.animacao_contador++;
+            (*jogo).animacao_contador++;
         } else {
             multicore_fifo_push_blocking(3);
-            jogo_global.animacao_contador = 0;
+            (*jogo).animacao_contador = 0;
             
             for(i = 0; i < 4; i++) {
                 flg_botao[i] = 0;
             }
             
-            transicao_estado(ESTADO_AGUARDANDO_INICIO);
+            transicao_estado(jogo, ESTADO_AGUARDANDO_INICIO);
         }
     }
 }
 
-
+// ================= MAIN =================
 int main(void) {
-    jogo_global.tamanho = 0;
-    jogo_global.rodando = 0;
-    jogo_global.aguardando_jogada = 0;
-    jogo_global.btn_p = 0;
-    jogo_global.flg_timeout = 0;
-    jogo_global.id_alarme = -1;
-    jogo_global.estado_atual = ESTADO_AGUARDANDO_INICIO;
-    jogo_global.indice_reproducao = 0;
-    jogo_global.n_jogada = 0;
-    jogo_global.btn_pressionado = 0;
-    jogo_global.animacao_contador = 0;
-    jogo_global.tempo_ultima_jogada = 0;
+    JogoState jogo;
+    
+    jogo.tamanho = 0;
+    jogo.rodando = 0;
+    jogo.aguardando_jogada = 0;
+    jogo.btn_p = 0;
+    jogo.flg_timeout = 0;
+    jogo.id_alarme = -1;
+    jogo.estado_atual = ESTADO_AGUARDANDO_INICIO;
+    jogo.indice_reproducao = 0;
+    jogo.n_jogada = 0;
+    jogo.btn_pressionado = 0;
+    jogo.animacao_contador = 0;
+    jogo.tempo_ultima_jogada = 0;
     
     for(int i = 0; i < MAX_SEQUENCIA; i++) {
-        jogo_global.sequencia[i] = 0;
+        jogo.sequencia[i] = 0;
     }
+    
+    ptr_jogo_irq = &jogo;
     
     stdio_init_all();
     botoes_init();
     multicore_launch_core1(core1_entry);
 
     while (true) {
-        maquina_estados();
+        maquina_estados(&jogo);
         sleep_ms(10);
     }
 
